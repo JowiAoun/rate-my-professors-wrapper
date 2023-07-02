895e818d1ae6cc055add30b86091afb9ac87f7b5
Jowi <83415433+JowiAoun@users.noreply.github.com>
2023-07-02 00:05:45 -0400

    Initializing project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+function fetchRmpByTeacher(prof: string) {
+  //! See how I can fetch the graphql data right away from the API instead of
+  //! having to parse through the entire HTML document (slower)
+}
+
+const getTeacher = async () => {
+  return 0;
+};
+
